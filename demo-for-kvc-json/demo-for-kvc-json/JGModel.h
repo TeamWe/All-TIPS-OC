84889754a8882e3c69528c03c4e1574882d64828
徐纪光 <xujiguang@xiyou-mobile-club.local>
2016-04-25 21:47:36 +0800

    json-1

---
+++ demo-for-kvc-json/demo-for-kvc-json/JGModel.h
@@ -0,0 +1,13 @@
+//
+//  JGModel.h
+//  demo-for-kvc-json
+//
+//  Created by 徐纪光 on 16/4/25.
+//  Copyright © 2016年 谭嘉麒. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JGModel : NSObject
+
+@end
---
+++ demo-for-kvc-json/demo-for-kvc-json/JGModel.m
@@ -0,0 +1,13 @@
+//
+//  JGModel.m
+//  demo-for-kvc-json
+//
+//  Created by 徐纪光 on 16/4/25.
+//  Copyright © 2016年 谭嘉麒. All rights reserved.
+//
+
+#import "JGModel.h"
+
+@implementation JGModel
+
+@end
